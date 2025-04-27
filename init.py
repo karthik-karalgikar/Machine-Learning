93b2e02ca48017d912a2226604304cf975dbfde8
karthik-karalgikar <karalgikarkarthik@gmail.com>
2025-04-28 03:04:00 +0530

    perceptron

--- init.py
+++ init.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
+from matplotlib import style
 from perceptron import MyPerceptron
+
+style.use('fivethirtyeight')
 
 df = pd.read_csv('breast_cancer_data.csv')
 df.replace('?', -99999, inplace=True)
@@ -19,3 +23,17 @@ clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 print(accuracy_score(y_test, y_pred))
 #0.9142857142857143
+
+X_vis = X_train[:, [0, 1]]  # first two features
+y_vis = y_train
+
+# Plot different classes with different colors
+for label, color in zip(np.unique(y_vis), ['red', 'blue']):
+    plt.scatter(X_vis[y_vis == label, 0], X_vis[y_vis == label, 1], 
+                label=f"Class {label}", color=color)
+
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.title('Breast Cancer Data (2 features)')
+plt.legend()
+plt.show()
